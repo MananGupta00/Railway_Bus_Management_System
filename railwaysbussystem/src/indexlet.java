65e5d02631ff1cad0387373c8b1280a58c288cd3
Manan Gupta <54077213+MananGupta00@users.noreply.github.com>
2020-04-07 01:41:56 +0530

    Add files via upload

--- railwaysbussystem/src/indexlet.java
+++ railwaysbussystem/src/indexlet.java
@@ -1,6 +1,10 @@
 
 
 import java.io.IOException;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
 import java.util.ArrayList;
 
 import javax.servlet.ServletException;
@@ -17,15 +21,51 @@ public class indexlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		ArrayList<String> list = new ArrayList<>();
-	    list.add("item1");
-	    list.add("item2");
-	    list.add("item3");
-	    String json = new Gson().toJson(list);
+		
+		String reqid=request.getParameter("id");
+		try{
+		Connection con = DatabaseConnection.initializeDatabase(); 
+		PrintWriter out = response.getWriter(); 
+		
+		if(reqid.equals("bus"))
+		{
+			PreparedStatement st = con.prepareStatement("select * from busadmin;");
+			ResultSet rs = st.executeQuery();
+			
+		    out.print("<table align='center'>");
+			out.print("<tr><td>Bus Number	</td><td>Bus Conductor	</td><td>Railway Station</td></tr>");
 
-	    response.setContentType("application/json");
-	    response.setCharacterEncoding("UTF-8");
-	    response.getWriter().write(json);
+			 while(rs.next()){	 
+				out.print("<tr><td>"+rs.getString(1)+"</td><td>"+rs.getString(2)+
+						"</td><td>"+rs.getString(3)+"</td></tr>");
+			 }
+			 
+			 out.print("</table>");
+			 st.close();
+			}
+		else if(reqid.equals("stop"))
+		{
+			PreparedStatement st = con.prepareStatement("select * from stops;");
+			ResultSet rs = st.executeQuery();
+			
+		    out.print("<table align='center'>");
+			out.print("<tr><td>STOP:</td></tr>");
+
+			 while(rs.next()){	 
+				out.print("<tr><td>"+rs.getString(1)+"</td></tr>");
+			 }
+			 
+			 out.print("</table>");
+			 st.close();
+			}
+		
+	    con.close();
+	    out.close();
+		}
+		catch(Exception e){
+			e.printStackTrace(); 
+		}
+		
 	}
 
 	
